2eb3822e3ae033ba081cd77c70f72f6acbcd4cfe
neliocarlos <100919012+neliocarlos@users.noreply.github.com>
2022-04-08 14:23:18 -0400

    Add files via upload

---
+++ SQLsaves/SQL_insert_into_intro.sql
@@ -0,0 +1,12 @@
+--SELECT * FROM Produto;
+
+INSERT INTO Produto(ID_Produto, Descricao_Produto, Preco_Produto)
+VALUES 
+(3, 'Fanta Laranja lata 150ml', 1.50),
+(4, 'Fanta Uva lata 150ml', 1.50),
+(5, 'Sprite lata 150ml', 1.50),
+(6, 'Pepsi lata 150ml', 1.50),
+(7, 'Pepsi Twist lata 150ml', 1.50),
+(8, 'Tubaína lata 150ml', 1.50),
+(9, 'Guaraná Antártica lata 150ml', 1.50),
+(10, 'Dolly lata 150ml', 1.50);
