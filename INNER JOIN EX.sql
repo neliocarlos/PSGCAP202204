9477e41f483a528489c3846d1978f41aa4fdda08
neliocarlos <100919012+neliocarlos@users.noreply.github.com>
2022-04-12 15:24:55 -0400

    Delete INNER JOIN EX.sql

--- INNER JOIN EX.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	Subcategoria.ID_Subcategoria,
-	Subcategoria.Descricao_Subcategoria AS Subcategoria,
-	Subcategoria.ID_Categoria,
-	Categoria.Descricao_Categoria AS Categoria,
-	Categoria.DataInclusao
-FROM Subcategoria
-INNER JOIN Categoria ON Categoria.ID_Categoria = Subcategoria.ID_Categoria;
