addae98c3c10615437ba85e6c0d6169c274dbadf
neliocarlos <100919012+neliocarlos@users.noreply.github.com>
2022-04-18 17:15:47 -0400

    Create Aula_Update_Alter_Add_18.04.sql

---
+++ SQLsaves/Aula_Update_Alter_Add_18.04.sql
@@ -0,0 +1,18 @@
+--SELECT * FROM Exercicio
+--WHERE (Situacao = 1);
+
+--SELECT * FROM Exercicio
+--WHERE Nota_Exercicio < 4;
+
+--UPDATE Exercicio
+--SET
+--	Situacao = 0,
+--	DataAlteracao = GETDATE(),
+--	DataExclusao = GETDATE()
+--WHERE Nota_Exercicio < 4;
+
+
+--ALTER TABLE Exercicio
+--	ADD 
+--		DataAlteracao DATETIME NULL,
+--		DataExclusao DATETIME NULL;
