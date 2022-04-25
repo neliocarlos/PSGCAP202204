03b7feaef840220cc6e398df387b8e6674a8821d
neliocarlos <100919012+neliocarlos@users.noreply.github.com>
2022-04-25 14:43:03 -0400

    Update CREATE TABLE Funcionarios.sql

--- AtacadoSQL202204/CREATE TABLE Funcionarios.sql
+++ AtacadoSQL202204/CREATE TABLE Funcionarios.sql
@@ -16,3 +16,20 @@ CREATE TABLE Funcionarios(
 )
 GO
 
+
+CREATE TABLE Funcionarios_Dados_Pessoais(
+	ID_Funcionario bigint NOT NULL,
+	Matricula_Funcionario bigint NOT NULL,
+	Nome_Funcionario varchar(50) NOT NULL,
+	Sobrenome_Funcionario varchar(50) NOT NULL,
+	Sexo_Funcionario char(1) NOT NULL,
+	Datanasc_Funcionario datetime NOT NULL,
+	Email_Funcionario varchar(MAX) NOT NULL,
+	ID_Pais int NOT NULL,
+	Situacao bit NULL DEFAULT 1,
+	DataInclusao datetime NULL DEFAULT getdate(),
+	DataAlteracao datetime NULL,
+	DataExclusao datetime NULL
+	CONSTRAINT PK_FuncDadosPessoais PRIMARY KEY (ID_Funcionario)
+)
+GO 
