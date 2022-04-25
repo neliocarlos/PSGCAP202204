2a14a7b5d6cf100445f7f2c588ac82e2a3aca980
neliocarlos <100919012+neliocarlos@users.noreply.github.com>
2022-04-25 17:19:53 -0400

    Create Aula_25.04.sql

---
+++ SQLsaves/Aula_25.04.sql
@@ -0,0 +1,9 @@
+--SELECT
+--day(getdate()) as 'Dia atual',
+--month(getdate()) as 'Mês atual',
+--year(getdate()) as 'Ano atual'
+
+--declare @mensagem varchar(max)
+--set @mensagem = 'OI MUNDO'
+--select @mensagem
+--go
